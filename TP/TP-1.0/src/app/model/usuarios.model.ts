a21c3223c20f5719c33993112afe26b9616eb669
AngelooMG <106643360+AngelooMG@users.noreply.github.com>
2023-04-30 22:15:56 -0500

    Update usuarios.model.ts

--- TP/TP-1.0/src/app/model/usuarios.model.ts
+++ TP/TP-1.0/src/app/model/usuarios.model.ts
@@ -1,9 +1,1 @@
-export class Users {
-  id: number = 0;
-  name: string = "";
-  lastname: string = "";
-  email: string = "";
-  age: number = 0;
-  district: string="";
-  profession: string = "";
-}
+
